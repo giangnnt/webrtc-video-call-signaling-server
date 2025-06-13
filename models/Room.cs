64d3ca4bfab34e7cdd33196b9110257e18216283
nguyengiang <giangnnt260703@gmail.com>
2025-06-13 09:42:56 +0700

    remove unuse model and service

--- models/Room.cs
+++
@@ -1,8 +0,0 @@
-public class Room
-{
-    public string Id { get; set; } = Guid.NewGuid().ToString();
-
-    public string? Name { get; set; }
-    public List<string> ConnectionIds { get; set; } = new List<string>();
-}
-
--- obj/Debug/net8.0/signaling-server-webrtc.AssemblyInfo.cs
+++ obj/Debug/net8.0/signaling-server-webrtc.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("signaling-server-webrtc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+980ba8c2b0f9175817e3dc176286ac4a96f84454")]
 [assembly: System.Reflection.AssemblyProductAttribute("signaling-server-webrtc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("signaling-server-webrtc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
